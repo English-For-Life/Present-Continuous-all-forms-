f676b8ecfa4ca03ef6ca4b8f852e1b468bc400c0
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-18 13:36:13 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,54 @@
+//Array of objects
+const quiz = [
+    {
+        q:"'___ you ___ okay?' 'Yes, I'm feeling fine.'",
+        options:["Do / feeling", "Are / feeling", "Are / feel", "- / feeling"],
+        answer: 1
+    },
+    {
+     q:"___ your brother ___ football today?",
+        options:["Is / playing", "Are / playing", "Does / play", "- / playing"],
+        answer: 0   
+    },
+    {
+        q:"___ it ___ outside?",
+        options:["Are / rain", "- / raining", "Is / raining", "Does / rains"],
+        answer: 2
+    },
+    {
+        q:"Mike and Rachel ___ breakfast at the moment.",
+        options:["is having", "have", "having", "are having"],
+        answer: 3
+    },
+    {
+        q:"Chandler, Monica and Joy ___ in the cafe now.",
+        options:["is sitting", "sitting", "are sitting", "sit"],
+        answer: 2
+    },
+    {
+        q:" 'The phone ___.' 'Answer, please!'",
+        options:["ringing", "is ringing", "ring", "are ringing"],
+        answer: 1
+    },
+    {
+        q:"I ___ , I am listening to music.",
+        options:["'m not reading", "am not read", "don't read", "isn't reading"],
+        answer: 0
+    },
+    {
+        q:"Look! They ___ their homework. ",
+        options:["aren't do", "isn't doing", "don't do", "aren't doing"],
+        answer: 3
+    },
+    {
+        q:"Their children ___ right now.",
+        options:["is swimming", "are swimming", "swimming", "swim"],
+        answer: 1
+    },
+    {
+        q:"We ___ . We ___ .",
+        options:["isn't cooking / is eating", "aren't cooking / eating", "aren't cooking / are eating", "aren't cook / are eating"],
+        answer: 2
+    }
+
+]
